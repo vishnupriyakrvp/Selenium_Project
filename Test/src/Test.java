ca69bf23029cbb646d684991a04441428d0733ec
Vishnu <priyav@dnb.com>
2016-01-13 18:36:19 -0800

    Added the basic selenium script

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,8 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver driver= new FirefoxDriver();
+		driver.get("www.amazon.ca");
 
 	}
 
