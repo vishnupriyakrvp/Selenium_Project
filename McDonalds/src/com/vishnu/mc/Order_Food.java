8dc91e9d63039e1da267a1ea851425628f3789a9
Vishnu <priyav@dnb.com>
2016-01-24 20:36:12 -0800

    Added order food for McDonalds site

--- McDonalds/src/com/vishnu/mc/Order_Food.java
+++ McDonalds/src/com/vishnu/mc/Order_Food.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 import com.testNg.vishnu.amazon_Login;
 
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,6 +14,7 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterTest;
@@ -20,6 +23,25 @@ public class Order_Food {
 	
 	public WebDriver driver;
 	Logger logger = Logger.getLogger(Order_Food.class.getName());
+	
+	
+	/*@Parameters({"browser", "driverPath"})
+		    @BeforeTest
+		    public void initDriver(String browser, @Optional("") String driverPath) throws Exception {
+		        System.out.println("You are testing on browser " + browser);
+		        browser = browser.toLowerCase();
+		        if (!driverPath.equals("")) {
+		            System.setProperty("webdriver.chrome.driver", driverPath);
+		        }
+		        if (browser.equals("chrome")) {        
+		            driver = new ChromeDriver();
+		        } else if (browser.equals("firefox")) {
+		            driver = new FirefoxDriver();
+		        } else {
+		            throw new RuntimeException("Please create a driver for " + browser);
+		        }
+		    }*/
+	
 	
 	 @BeforeTest
 	  public void beforeTest() {
